3ca245a377f42fe7e7434d85e6373092f15262eb
Harry Le <harry@vinova.sg>
2017-01-05 16:25:23 +0700

    init project

---
+++ EazyKotlin/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+c
+3app/src/main/java/vn/eazy/kotlin/ui/MainActivity.kt,0/0/001f7ba83f99dd9a52abc9582c13af651fb673ef
---
+++ EazyKotlin/app/src/main/java/vn/eazy/kotlin/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package vn.eazy.kotlin.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import vn.eazy.kotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
